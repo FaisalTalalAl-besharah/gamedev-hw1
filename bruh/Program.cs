7165586d991cf67b4e443c605a4bb0ba39280cfb
FaisalTalalAl-besharah <ff162611@gmail.com>
2022-03-11 20:02:50 +0300

    summary

---
+++ bruh/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace bruh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] HealthArray = { "damaged", "boosted" };
+            {
+                void damaged()
+                {
+                    int health = 100;
+                    int damaged = 20;
+                    Console.WriteLine("Damaged health : {0}", health - damaged);
+                }
+                damaged();
+                void boosted()
+                {
+                    int health2 = 80;
+                    int boost = 10;
+                    Console.WriteLine("Boosted health : {0}", health2 + boost);
+                }
+                boosted();
+                Console.WriteLine("80 is less than 90");
+            }
+        }
+    }
+}
